18c541aa812086d718329c4911bd8f9555f5fc64
Jacob Mason <jacob@jacobmason.net>
2016-06-10 11:08:07 -0500

    Fixes Net installers downloading Android SDK packages

--- data/get_sdk_packages.bat
+++ data/get_sdk_packages.bat
@@ -1,1 +1,1 @@
-echo y | "%LOCALAPPDATA%\Android\android-sdk\tools\android.bat" --silent update sdk --all --no-ui --filter "build-tools-21.1.2,extra-google-usb_driver,android-19,platform-tools"
+echo y | "%ANDROID_HOME%\tools\android.bat" --silent update sdk --all --no-ui --filter "build-tools-21.1.2,extra-google-usb_driver,android-19,platform-tools"
